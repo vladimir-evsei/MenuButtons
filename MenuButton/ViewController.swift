69b885bd78b39e2809cd9f1469a7edf9708fa603
vladimir-evsei <evsei.vladmr@gmail.com>
2016-09-01 17:30:10 +0300

    Initial Commit

---
+++ MenuButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuButton
+//
+//  Created by Evsei, Vladimir on 9/1/16.
+//  Copyright © 2016 Evsei, Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
